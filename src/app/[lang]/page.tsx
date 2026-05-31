a14156b9feb289e487ecff00421951fbbfe966bb
Ahmed khaled <105175448+AhmedkhaledSobhi@users.noreply.github.com>
2026-05-31 18:38:34 +0300

    handle some change

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import MySVG from "@/SVG/MySVG";
+import { GraduationCap } from "lucide-react";
 import Image from "next/image";
 import { redirect, useRouter } from "next/navigation";
 import { useState } from "react";
@@ -50,53 +51,40 @@ export default function Home() {
     },
   ];
   return (
-    // <section className={`bg-amber-400 w-full h-screen flex items-center justify-center text-center`} >
-    //   <div className="">
-    //     <div className="flex flex-row-reverse ">
-    //       <h1 className="text-xl font-medium">Welcome to the homepage of the (Center Education) platform</h1>
-    //       <Image
-    //         src={MySVG.logoSm}
-    //         alt="Center_Education logo"
-    //         className="mx-2"
-    //         style={{ width: "50px", height: "50px" }}
-    //       />
-    //     </div>
-    //     <p className="text-xl font-medium my-2.5">This is the main content of the home page</p>
-    //     <button
-    //       className="rounded-[10px] py-2.5 px-6 my-3.5 text-white bg-blue-700 hover:bg-blue-800 cursor-pointer"
-    //       onClick={() => { router.push('/login') }}
-    //     >
-    //       Login
-    //     </button>
-    //     <button
-    //       className="rounded-[10px] py-2.5 px-6 my-3.5 text-white bg-blue-700 hover:bg-blue-800 cursor-pointer"
-    //       onClick={() => { router.push('/dashboard') }}
-    //     >
-    //       Dashboard
-    //     </button>
-
-    //   </div>
-    // </section>
     <main className="bg-slate-50 text-slate-900">
-
       {/* HEADER */}
-      <header className="sticky top-0 z-50 bg-white border-b">
+      <header className="sticky top-0 z-50 bg-white border-b border-blue-700">
         <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
-          <h1 className="text-xl font-bold text-blue-700">Center Academy</h1>
+          <div 
+            className="flex items-center gap-0 cursor-pointer"
+            onClick={() => { router.push('/home') }}
+          >
+            <div className="w-15 bg-white/10 rounded-xl flex items-center justify-center">
+              <GraduationCap size={39} 
+                className="text-[#1447e6]"
+              />
+            </div>
 
-          <nav className="hidden md:flex gap-6 text-sm text-slate-600">
-            <a href="#">Home</a>
-            <a href="#">Courses</a>
-            <a href="#">Programs</a>
-            <a href="#">Instructors</a>
-            <a href="#">Contact</a>
-          </nav>
+            <div>
+              <h1 className="text-3xl font-bold tracking-wide text-blue-700">
+                CENTER
+              </h1>
 
+              <p className="text-xl tracking-[4px] text-blue-700">
+                ACADEMY
+              </p>
+            </div>
+          </div>
           <div className="flex gap-3">
-            <button className="text-sm px-4 py-2">Login</button>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-xl text-sm hover:bg-blue-700">
-              Get Started
+            <button
+              className="border-2 border-blue-800 text-blue-800 font-bold px-4 py-2 cursor-pointer hover:bg-blue-50 transition rounded-xl"
+              onClick={() => { router.push('/login') }}
+            >
+              Login
             </button>
+            {/* <button className="bg-blue-600 text-white px-4 py-2 rounded-xl text-sm hover:bg-blue-700 cursor-pointer">
+              Get Started
+            </button> */}
           </div>
         </div>
       </header>
@@ -123,7 +111,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="bg-gradient-to-br from-blue-100 to-blue-50 rounded-2xl h-72 flex items-center justify-center text-blue-600 font-semibold">
+        <div className="bg-linear-to-br from-blue-100 to-blue-50 rounded-2xl h-72 flex items-center justify-center text-blue-600 font-semibold">
           Platform Preview
         </div>
       </section>
@@ -227,20 +215,25 @@ export default function Home() {
                     : "bg-white"
                   }`}
               >
-                <h3 className="font-semibold text-lg">{plan.name}</h3>
-                <p className="text-2xl font-bold text-blue-700 mt-2">
-                  {plan.price}
-                </p>
+                <div className="flex flex-col h-56 .5 justify-between">
+                  <div >
+                    <h3 className="font-semibold text-lg">{plan.name}</h3>
+                    <p className="text-2xl font-bold text-blue-700 mt-2">
+                      {plan.price}
+                    </p>
 
-                <ul className="mt-4 space-y-2 text-sm text-slate-600">
-                  {plan.features.map((f) => (
-                    <li key={f}>✔ {f}</li>
-                  ))}
-                </ul>
+                    <ul className="mt-4 space-y-2 text-sm text-slate-600">
+                      {plan.features.map((f) => (
+                        <li key={f}>✔ {f}</li>
+                      ))}
+                    </ul>
+                  </div>
+                
+                  <button className="mt- 6 mt-auto w-full bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700">
+                    Select Plan
+                  </button>
+                </div>
 
-                <button className="mt-6 w-full bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700">
-                  Select Plan
-                </button>
               </div>
             ))}
           </div>
